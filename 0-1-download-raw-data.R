7692ce0dd274b821590fb3bbfca4d99b562278ec
Mauricio Vargas <mvargas@dcc.uchile.cl>
2018-08-21 17:50:26 -0300

    fixes doparallel problem and adds alternatives for windows

--- 0-1-download-raw-data.R
+++ 0-1-download-raw-data.R
@@ -1,11 +1,20 @@
 # Open oec-yearly-data.Rproj before running this function
 
+# detect system -----------------------------------------------------------
+
+operating_system <- Sys.info()[['sysname']]
+
 # packages ----------------------------------------------------------------
 
 if (!require("pacman")) install.packages("pacman")
-p_load(data.table, dplyr, jsonlite, doParallel)
 
-download <- function() {
+if (operating_system != "Windows") {
+  p_load(data.table, dplyr, jsonlite, doParallel)
+} else {
+  p_load(data.table, dplyr, jsonlite)
+}
+
+download <- function(n_cores = 4) {
   # user parameters ---------------------------------------------------------
 
   message(
@@ -24,34 +33,29 @@ download <- function() {
     "\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n"
   )
   readline(prompt = "Press [enter] to continue")
-  
+
   download_data_rev <- menu(
     c("HS rev 1992", "HS rev 1996", "HS rev 2002", "HS rev 2007", "SITC rev 2"),
-    title = "Select dataset:", 
-    graphics = T
+    title = "Select dataset:",
+    graphics = F
   )
 
   remove_old_files <- menu(
-    c("yes", "no"), 
+    c("yes", "no"),
     title = "Remove old files (y/n):",
-    graphics = T
+    graphics = F
   )
-  
-  token <- menu(
-    c("yes", "no"), 
-    title = "Have you stored your token safely in your .Renviron file?",
-    graphics = T
-  )
-  
-  stopifnot(token == 1)
-  
-  token <- Sys.getenv("token")
-  
-  # multicore parameters ----------------------------------------------------
 
-  #n_cores <- 4
-  n_cores <- detectCores() - 1
-  
+  token <- menu(
+    c("yes", "no"),
+    title = "Have you stored your token safely in your .Renviron file?",
+    graphics = F
+  )
+
+  stopifnot(token == 1)
+
+  token <- Sys.getenv("token")
+
   # revisions ---------------------------------------------------------------
 
   download_data_rev1992 <- ifelse(download_data_rev == 1, T, F)
@@ -59,7 +63,7 @@ download <- function() {
   download_data_rev2002 <- ifelse(download_data_rev == 3, T, F)
   download_data_rev2007 <- ifelse(download_data_rev == 4, T, F)
   download_data_rev2 <- ifelse(download_data_rev == 5, T, F)
-  
+
   # HS 1992 -----------------------------------------------------------------
 
   if (download_data_rev1992 == T) {
@@ -67,62 +71,62 @@ download <- function() {
     rev <- 1992
     classification <- "H0"
   }
-  
+
   # HS 1996 -----------------------------------------------------------------
-  
+
   if (download_data_rev1996 == T) {
     years <- 1996:2016
     rev <- 1996
     classification <- "H1"
   }
-  
+
   # HS 2002 -----------------------------------------------------------------
-  
+
   if (download_data_rev2002 == T) {
     years <- 2002:2016
     rev <- 2002
     classification <- "H2"
   }
-  
+
   # HS 2007 -----------------------------------------------------------------
-  
+
   if (download_data_rev2007 == T) {
     years <- 2007:2016
     rev <- 2007
     classification <- "H3"
   }
-  
+
   # SITC rev 2 --------------------------------------------------------------
-  
+
   if (download_data_rev2 == T) {
     years <- 1990:2016
     rev <- 2
     classification <- "S2"
   }
-  
+
   # output dirs -------------------------------------------------------------
-  
+
   raw_dir <- "1-1-raw-data"
-  
+
   if (download_data_rev < 5) {
     classification_dir <- sprintf("%s/hs-rev%s", raw_dir, rev)
   } else {
     classification_dir <- sprintf("%s/sitc-rev%s", raw_dir, rev)
   }
-  
+
   try(dir.create(raw_dir))
   try(dir.create(classification_dir))
-  
+
   zip_dir <- paste0(classification_dir, "/zip/")
-  
+
   try(dir.create(zip_dir))
-  
+
   # helpers -----------------------------------------------------------------
-  
+
   data_downloading <- function(t) {
-    if (remove_old_files == 1 & 
-        (links$local_file_date[[t]] < links$server_file_date[[t]]) & 
-        !is.na(links$old_file[[t]])) {
+    if (remove_old_files == 1 &
+      (links$local_file_date[[t]] < links$server_file_date[[t]]) &
+      !is.na(links$old_file[[t]])) {
       try(file.remove(links$old_file[[t]]))
     }
     if (!file.exists(links$new_file[[t]])) {
@@ -131,24 +135,34 @@ download <- function() {
         Sys.sleep(sample(seq(5, 10, by = 1), 1))
         try(
           download.file(links$url[[t]],
-                        links$new_file[[t]],
-                        method = "wget",
-                        quiet = T,
-                        extra = "--no-check-certificate")
+            links$new_file[[t]],
+            method = "wget",
+            quiet = T,
+            extra = "--no-check-certificate"
+          )
         )
-        
-        if(file.size(links$new_file[[t]]) == 0) { fs <- 1 } else { fs <- 0 }
-        
+
+        if (file.size(links$new_file[[t]]) == 0) {
+          fs <- 1
+        } else {
+          fs <- 0
+        }
+
         while (fs > 0) {
           try(
             download.file(links$url[[t]],
-                          links$new_file[[t]],
-                          method = "wget",
-                          quiet = T,
-                          extra = "--no-check-certificate")
+              links$new_file[[t]],
+              method = "wget",
+              quiet = T,
+              extra = "--no-check-certificate"
+            )
           )
-          
-          if (file.size(links$new_file[[t]]) == 0) { fs <- fs + 1 } else { fs <- 0 }
+
+          if (file.size(links$new_file[[t]]) == 0) {
+            fs <- fs + 1
+          } else {
+            fs <- 0
+          }
         }
       } else {
         message(paste(
@@ -160,13 +174,13 @@ download <- function() {
       message(paste(links$new_file[[t]], "exists. Skiping."))
     }
   }
-  
+
   # download data -----------------------------------------------------------
-  
+
   try(
     old_file <- list.files(classification_dir, pattern = "downloaded", full.names = T)
   )
-  
+
   if (length(old_file) > 0) {
     old_links <- as_tibble(fread(old_file)) %>%
       mutate(
@@ -176,22 +190,24 @@ download <- function() {
       ) %>%
       rename(old_file = file)
   }
-  
-  links <- tibble(year = years, 
-                  url = paste0(
-                    "https://comtrade.un.org/api/get/bulk/C/A/",
-                    year,
-                    "/ALL/",
-                    classification,
-                    "?token=",
-                    token
-                  ), 
-                  file = NA)
-  
-  files <- fromJSON(sprintf("https://comtrade.un.org/api/refs/da/bulk?freq=A&r=ALL&px=%s&token=%s", classification, token)) %>% 
-    filter(ps %in% years) %>% 
+
+  links <- tibble(
+    year = years,
+    url = paste0(
+      "https://comtrade.un.org/api/get/bulk/C/A/",
+      year,
+      "/ALL/",
+      classification,
+      "?token=",
+      token
+    ),
+    file = NA
+  )
+
+  files <- fromJSON(sprintf("https://comtrade.un.org/api/refs/da/bulk?freq=A&r=ALL&px=%s&token=%s", classification, token)) %>%
+    filter(ps %in% years) %>%
     arrange(ps)
-  
+
   if (exists("old_links")) {
     links <- links %>%
       mutate(file = paste0(zip_dir, files$name)) %>%
@@ -201,13 +217,15 @@ download <- function() {
         server_file_date = as.Date(server_file_date, "%Y%m%d")
       ) %>%
       left_join(old_links %>% select(-url), by = "year") %>%
-      rename(new_file = file) %>% 
+      rename(new_file = file) %>%
       mutate(
         server_file_date = as.Date(
-          ifelse(is.na(local_file_date), server_file_date + 1, server_file_date), origin = "1970-01-01"
+          ifelse(is.na(local_file_date), server_file_date + 1, server_file_date),
+          origin = "1970-01-01"
         ),
         local_file_date = as.Date(
-          ifelse(is.na(local_file_date), server_file_date - 1, local_file_date), origin = "1970-01-01"
+          ifelse(is.na(local_file_date), server_file_date - 1, local_file_date),
+          origin = "1970-01-01"
         )
       )
   } else {
@@ -218,19 +236,23 @@ download <- function() {
         server_file_date = gsub("_fmt.*", "", server_file_date),
         server_file_date = as.Date(server_file_date, "%Y%m%d")
       ) %>%
-      mutate(old_file = NA) %>% # trick in case there are no old files 
-      mutate(local_file_date = server_file_date) %>% 
-      mutate(server_file_date = as.Date(server_file_date + 1, origin = "1970-01-01")) %>% # trick in case there are no old files 
+      mutate(old_file = NA) %>% # trick in case there are no old files
+      mutate(local_file_date = server_file_date) %>%
+      mutate(server_file_date = as.Date(server_file_date + 1, origin = "1970-01-01")) %>% # trick in case there are no old files
       rename(new_file = file)
   }
-  
-  mclapply(1:length(years), data_downloading, mc.cores = n_cores)
+
+  if (operating_system != "Windows") {
+    mclapply(1:length(years), data_downloading, mc.cores = n_cores)
+  } else {
+    lapply(1:length(years), data_downloading)
+  }
   
   links <- links %>%
     mutate(url = gsub(token, "REPLACE_TOKEN", url)) %>%
     select(year, url, new_file, local_file_date) %>%
     rename(file = new_file)
-  
+
   try(file.remove(old_file))
   fwrite(links, paste0(classification_dir, "/downloaded-files-", Sys.Date(), ".csv"))
 }
--- 0-2-process-clean-data.R
+++ 0-2-process-clean-data.R
@@ -1,11 +1,20 @@
 # Open oec-yearly-data.Rproj before running this function
 
+# detect system -----------------------------------------------------------
+
+operating_system <- Sys.info()[['sysname']]
+
 # packages ----------------------------------------------------------------
 
 if (!require("pacman")) install.packages("pacman")
-p_load(data.table, feather, dplyr, tidyr, janitor, doParallel)
 
-clean <- function(compress_output = T) {
+if (operating_system != "Windows") {
+  p_load(data.table, feather, dplyr, tidyr, janitor, doParallel)
+} else {
+  p_load(data.table, feather, dplyr, tidyr, janitor)
+}
+
+clean <- function(compress_output = T, n_cores = 4) {
   # user parameters ---------------------------------------------------------
 
   message(
@@ -24,36 +33,49 @@ clean <- function(compress_output = T) {
     "\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n"
   )
   readline(prompt = "Press [enter] to continue")
-  
+
   dataset <- menu(
     c("HS rev 1992", "HS rev 1996", "HS rev 2002", "HS rev 2007", "SITC rev 2"),
-    title = "Select dataset:", 
-    graphics = T
+    title = "Select dataset:",
+    graphics = F
   )
   
-  # multicore parameters ----------------------------------------------------
-
-  #n_cores <- 4
-  n_cores <- ceiling(detectCores() / 2)
-  
   # years by classification -------------------------------------------------
-  
-  if (dataset < 5) { classification <- "hs" } else { classification <- "sitc" }
-  if (dataset == 1) { rev <- 1992 }
-  if (dataset == 2) { rev <- 1996 }
-  if (dataset == 3) { rev <- 2002 }
-  if (dataset == 4) { rev <- 2007 }
-  if (dataset == 5) { rev <- 2 }
-  
-  if (classification == "sitc" & rev == 2) { years <- 1990:2016 }
-  if (classification == "hs") { years <- rev:2016 }
-  
+
+  if (dataset < 5) {
+    classification <- "hs"
+  } else {
+    classification <- "sitc"
+  }
+  if (dataset == 1) {
+    rev <- 1992
+  }
+  if (dataset == 2) {
+    rev <- 1996
+  }
+  if (dataset == 3) {
+    rev <- 2002
+  }
+  if (dataset == 4) {
+    rev <- 2007
+  }
+  if (dataset == 5) {
+    rev <- 2
+  }
+
+  if (classification == "sitc" & rev == 2) {
+    years <- 1990:2016
+  }
+  if (classification == "hs") {
+    years <- rev:2016
+  }
+
   # number of digits --------------------------------------------------------
-  
+
   J <- ifelse(classification == "sitc", 4, 6)
-  
+
   # input dirs --------------------------------------------------------------
-  
+
   raw_dir <- "1-1-raw-data"
   raw_zip_dir <- sprintf("%s/%s-rev%s/zip", raw_dir, classification, rev)
   raw_csv_dir <- sprintf("%s/%s-rev%s/csv", raw_dir, classification, rev)
@@ -64,13 +86,13 @@ clean <- function(compress_output = T) {
   rev_dir <- sprintf("%s/%s-rev%s", clean_dir, classification, rev)
   try(dir.create(clean_dir))
   try(dir.create(rev_dir))
-  
+
   # helpers -----------------------------------------------------------------
 
   messageline <- function() {
     message(rep("-", 60))
   }
-  
+
   extract <- function(t) {
     if (file.exists(raw_csv_list[[t]])) {
       messageline()
@@ -81,23 +103,24 @@ clean <- function(compress_output = T) {
       system(sprintf("7z e -aos %s -oc:%s", raw_zip_list[[t]], raw_csv_dir))
     }
   }
-  
+
   fread2 <- function(x) {
     messageline()
     message("function fread2")
     message("x: ", x)
     as_tibble(fread(
-      x, colClasses = list(
+      x,
+      colClasses = list(
         character = c("Commodity Code"),
         numeric = c("Trade Value (US$)")
       )
     )) %>% clean_names()
   }
-  
+
   file.remove2 <- function(x) {
     try(file.remove(x))
   }
-  
+
   compress <- function(t) {
     if (file.exists(clean_zip_list[[t]])) {
       message("The compressed files exist. Skipping.")
@@ -107,130 +130,132 @@ clean <- function(compress_output = T) {
       file.remove2(clean_feather_list[[t]])
     }
   }
-  
+
   # list input files --------------------------------------------------------
 
   raw_zip_list <-
-    list.files(path = raw_zip_dir,
-               pattern = "\\.zip",
-               full.names = T) %>%
+    list.files(
+      path = raw_zip_dir,
+      pattern = "\\.zip",
+      full.names = T
+    ) %>%
     grep(paste(paste0("ps-", years), collapse = "|"), ., value = TRUE)
-  
+
   raw_csv_list <- raw_zip_list %>% gsub("zip", "csv", .)
-  
+
   # list output files -------------------------------------------------------
 
   clean_feather_list <- sprintf("%s/%s-rev%s-%s.feather", rev_dir, classification, rev, years)
 
   clean_zip_list <- clean_feather_list %>%
     gsub("feather", "zip", .)
-  
+
   # uncompress input --------------------------------------------------------
 
-  mclapply(1:length(raw_csv_list), extract, mc.cores = n_cores)
-  
+  if (operating_system != "Windows") {
+    mclapply(1:length(raw_csv_list), extract, mc.cores = n_cores)
+  } else {
+    lapply(1:length(raw_csv_list), extract)
+  }
   # create tidy datasets ----------------------------------------------------
 
   messageline()
   message("Rearranging files. Please wait...")
-  
+
   # See Anderson & van Wincoop, 2004, Hummels, 2006 and Gaulier & Zignago, 2010 for 8% consistency
   cif_fob_rate <- 1.08
-  
-  cl <- makeCluster(n_cores, type = "FORK")
-  registerDoParallel(cores = n_cores)
-  
-  foreach (t = 1:length(raw_csv_list)) %dopar% {
+
+  compute_tidy_data <- function(t) {
     if (!file.exists(clean_feather_list[[t]])) {
       messageline()
       message(paste("Dividing year", years[[t]]))
-      
+
       # clean data --------------------------------------------------------------
 
-      clean_feather <- fread2(raw_csv_list[[t]]) %>% 
-        rename(trade_value_usd = trade_value_us) %>% 
-        select(trade_flow, reporter_iso, partner_iso, aggregate_level, commodity_code, trade_value_usd) %>% 
-        filter(aggregate_level %in% J) %>% 
+      clean_feather <- fread2(raw_csv_list[[t]]) %>%
+        rename(trade_value_usd = trade_value_us) %>%
+        select(trade_flow, reporter_iso, partner_iso, aggregate_level, commodity_code, trade_value_usd) %>%
+        filter(aggregate_level %in% J) %>%
         filter(trade_flow == "Export" | trade_flow == "Import") %>%
         filter(
           !is.na(reporter_iso),
           !is.na(partner_iso),
-          reporter_iso != "", 
+          reporter_iso != "",
           partner_iso != "",
-          reporter_iso != " ", 
+          reporter_iso != " ",
           partner_iso != " "
         ) %>%
         mutate(
           reporter_iso = tolower(reporter_iso),
           partner_iso = tolower(partner_iso)
-        ) %>% 
+        ) %>%
         filter(
-          reporter_iso != "sxm", 
+          reporter_iso != "sxm",
           partner_iso != "sxm", # San Marino is "smr" and not "sxm"
-          reporter_iso != "wld", 
+          reporter_iso != "wld",
           partner_iso != "wld" # the World (wld) is not needed as the OEC aggregates and computes total trade
         )
-      
+
       # exports data ------------------------------------------------------------
-      
-      exports <- clean_feather %>% 
-        filter(trade_flow == "Export") %>% 
-        unite(pairs, reporter_iso, partner_iso, sep = "_", remove = F) %>% 
-        rename(export_usd = trade_value_usd) %>% 
+
+      exports <- clean_feather %>%
+        filter(trade_flow == "Export") %>%
+        unite(pairs, reporter_iso, partner_iso, sep = "_", remove = F) %>%
+        rename(export_usd = trade_value_usd) %>%
         select(pairs, commodity_code, export_usd)
-      
-      exports_mirrored <- clean_feather %>% 
-        filter(trade_flow == "Import") %>% 
-        unite(pairs, partner_iso, reporter_iso, sep = "_", remove = F) %>% 
-        rename(export_usd_mirrored = trade_value_usd) %>% 
+
+      exports_mirrored <- clean_feather %>%
+        filter(trade_flow == "Import") %>%
+        unite(pairs, partner_iso, reporter_iso, sep = "_", remove = F) %>%
+        rename(export_usd_mirrored = trade_value_usd) %>%
         select(pairs, commodity_code, export_usd_mirrored)
-      
-      exports_model <- exports %>% 
-        full_join(exports_mirrored, by = c("pairs", "commodity_code")) %>% 
-        mutate(export_usd = round(export_usd, 0)) %>% 
-        mutate(export_usd_mirrored = round(export_usd_mirrored / cif_fob_rate, 0)) %>% 
-        rowwise() %>% 
-        mutate(export_usd = max(export_usd, export_usd_mirrored, na.rm = T)) %>% 
-        select(-matches("mirrored")) %>% 
+
+      exports_model <- exports %>%
+        full_join(exports_mirrored, by = c("pairs", "commodity_code")) %>%
+        mutate(export_usd = round(export_usd, 0)) %>%
+        mutate(export_usd_mirrored = round(export_usd_mirrored / cif_fob_rate, 0)) %>%
+        rowwise() %>%
+        mutate(export_usd = max(export_usd, export_usd_mirrored, na.rm = T)) %>%
+        select(-matches("mirrored")) %>%
         ungroup()
-      
+
       rm(exports, exports_mirrored)
-      
+
       # imports data ------------------------------------------------------------
-      
-      imports <- clean_feather %>% 
-        filter(trade_flow == "Import") %>% 
-        unite(pairs, reporter_iso, partner_iso, sep = "_", remove = F) %>% 
-        rename(import_usd = trade_value_usd) %>% 
+
+      imports <- clean_feather %>%
+        filter(trade_flow == "Import") %>%
+        unite(pairs, reporter_iso, partner_iso, sep = "_", remove = F) %>%
+        rename(import_usd = trade_value_usd) %>%
         select(pairs, commodity_code, import_usd)
-      
-      imports_mirrored <- clean_feather %>% 
-        filter(trade_flow == "Export") %>% 
-        unite(pairs, partner_iso, reporter_iso, sep = "_", remove = F) %>% 
-        rename(import_usd_mirrored = trade_value_usd) %>% 
+
+      imports_mirrored <- clean_feather %>%
+        filter(trade_flow == "Export") %>%
+        unite(pairs, partner_iso, reporter_iso, sep = "_", remove = F) %>%
+        rename(import_usd_mirrored = trade_value_usd) %>%
         select(pairs, commodity_code, import_usd_mirrored)
-      
-      imports_model <- imports %>% 
-        full_join(imports_mirrored, by = c("pairs", "commodity_code")) %>% 
-        mutate(import_usd = round(import_usd, 0)) %>% 
-        mutate(import_usd_mirrored = round(import_usd_mirrored / cif_fob_rate, 0)) %>% 
-        rowwise() %>% 
-        mutate(import_usd = max(import_usd, import_usd_mirrored, na.rm = T)) %>% 
-        select(pairs, commodity_code, import_usd) %>% 
+
+      imports_model <- imports %>%
+        full_join(imports_mirrored, by = c("pairs", "commodity_code")) %>%
+        mutate(import_usd = round(import_usd, 0)) %>%
+        mutate(import_usd_mirrored = round(import_usd_mirrored / cif_fob_rate, 0)) %>%
+        rowwise() %>%
+        mutate(import_usd = max(import_usd, import_usd_mirrored, na.rm = T)) %>%
+        select(pairs, commodity_code, import_usd) %>%
         ungroup()
-      
+
       rm(imports, imports_mirrored, clean_feather)
-      
+
       # trade data --------------------------------------------------------------
-      
-      clean_feather <- exports_model %>% 
-        full_join(imports_model) %>% 
-        separate(pairs, c("reporter_iso", "partner_iso"), sep = "_") %>% 
+
+      clean_feather <- exports_model %>%
+        full_join(imports_model) %>%
+        separate(pairs, c("reporter_iso", "partner_iso"), sep = "_") %>%
         mutate(year = years[[t]]) %>%
         select(year, everything())
-      
+
       write_feather(clean_feather, clean_feather_list[[t]])
-      
+
       rm(clean_feather)
     } else {
       messageline()
@@ -238,16 +263,23 @@ clean <- function(compress_output = T) {
     }
     rm(check_files)
   }
-  
-  stopCluster(cl)
-  rm(cl)
-  
-  # compress output ---------------------------------------------------------
-  
-  if(compress_output == T) {
-    mclapply(1:length(clean_zip_list), compress, mc.cores = n_cores)
+
+  if (operating_system != "Windows") {
+    mclapply(1:length(raw_csv_list), compute_tidy_data, mc.cores = n_cores)
+  } else {
+    lapply(1:length(raw_csv_list), compute_tidy_data)
   }
   
+  # compress output ---------------------------------------------------------
+
+  if (compress_output == T) {
+    if (operating_system != "Windows") {
+      mclapply(1:length(clean_zip_list), compress, mc.cores = n_cores)
+    } else {
+      lapply(1:length(clean_zip_list), compress)
+    }
+  }
+
   unlink(raw_csv_dir, recursive = T)
   rm(raw_zip_list, raw_csv_list, clean_feather_list, clean_zip_list)
 }
