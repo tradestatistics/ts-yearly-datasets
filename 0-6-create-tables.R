6ee446184dac22807815812d588e2c3adbee2558
Mauricio Vargas <mvargas@dcc.uchile.cl>
2018-12-08 16:48:18 -0300

    show GNU license terms on all scripts

--- 0-6-create-tables.R
+++ 0-6-create-tables.R
@@ -1,24 +1,15 @@
 # Open oec-yearly-data.Rproj before running this function
 
 tables <- function(n_cores = 2) {
-  # user parameters ---------------------------------------------------------
+  # messages ----------------------------------------------------------------
   
-  message(
-    "This function takes data obtained from UN Comtrade by using download functions in this project and creates tidy datasets ready to be added to the OEC"
-  )
-  message("\nCopyright (c) 2018, Mauricio \"Pacha\" Vargas\n")
-  readline(prompt = "Press [enter] to continue")
-  message("\nThe MIT License\n")
-  message(
-    "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:"
-  )
-  message(
-    "\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software."
-  )
-  message(
-    "\nTHE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n"
-  )
-  readline(prompt = "Press [enter] to continue")
+  message("\nCopyright (C) 2018, Mauricio \"Pacha\" Vargas\n")
+  message("This file is part of Open Trade Statistics project")
+  message("\nThe scripts within this project are released under GNU General Public License 3.0")
+  message("This program comes with ABSOLUTELY NO WARRANTY.")
+  message("This is free software, and you are welcome to redistribute it under certain conditions.\n")
+  message("See https://github.com/tradestatistics/ts-yearly-datasets/LICENSE for the details\n")
+  readline(prompt = "Press [enter] to continue if and only if you agree to the license terms")
   
   # helpers -----------------------------------------------------------------
   
